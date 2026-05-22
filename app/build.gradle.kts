d95da37082a278e1a7f9b16cc5a5567a4a31bcbc
Sunil Shahid <thesunilshahid@gmail.com>
2026-05-22 19:21:22 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,19 +35,8 @@ android {
       keyAlias = "androiddebugkey"
       keyPassword = "android"
     }
-  }
+  
 
-  buildTypes {
-    release {
-      isCrunchPngs = false
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
-    }
-    debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
-    }
-  }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
